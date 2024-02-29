645195aa7be56d552b244bcb6bd8bf2910df9033
Eric Silva <ericfelipedev@gmail.com>
2024-02-28 22:05:36 -0300

    0001

--- api/routers/user.js
+++ api/routers/user.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("./test", (req, res) => {
+router.get("/teste", (req, res) => {
     res.status(200).json({msg:"funcionando"})
 })
 
